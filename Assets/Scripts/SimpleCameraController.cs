90c63d274cb36069c7a31a89e0446b8ca4e7adf6
YetiYetYet <alexissbehier22@gmail.com>
2020-11-12 22:49:49 +0100

    Add MenuManager and some basic audio

--- Assets/Scripts/SimpleCameraController.cs
+++ Assets/Scripts/SimpleCameraController.cs
@@ -12,52 +12,52 @@ namespace UnityTemplateProjects
     {
         class CameraState
         {
-            public float yaw;
-            public float pitch;
-            public float roll;
-            public float x;
-            public float y;
-            public float z;
+            public float Yaw;
+            public float Pitch;
+            public float Roll;
+            public float X;
+            public float Y;
+            public float Z;
 
             public void SetFromTransform(Transform t)
             {
-                pitch = t.eulerAngles.x;
-                yaw = t.eulerAngles.y;
-                roll = t.eulerAngles.z;
-                x = t.position.x;
-                y = t.position.y;
-                z = t.position.z;
+                Pitch = t.eulerAngles.x;
+                Yaw = t.eulerAngles.y;
+                Roll = t.eulerAngles.z;
+                X = t.position.x;
+                Y = t.position.y;
+                Z = t.position.z;
             }
 
             public void Translate(Vector3 translation)
             {
-                Vector3 rotatedTranslation = Quaternion.Euler(pitch, yaw, roll) * translation;
+                Vector3 rotatedTranslation = Quaternion.Euler(Pitch, Yaw, Roll) * translation;
 
-                x += rotatedTranslation.x;
-                y += rotatedTranslation.y;
-                z += rotatedTranslation.z;
+                X += rotatedTranslation.x;
+                Y += rotatedTranslation.y;
+                Z += rotatedTranslation.z;
             }
 
             public void LerpTowards(CameraState target, float positionLerpPct, float rotationLerpPct)
             {
-                yaw = Mathf.Lerp(yaw, target.yaw, rotationLerpPct);
-                pitch = Mathf.Lerp(pitch, target.pitch, rotationLerpPct);
-                roll = Mathf.Lerp(roll, target.roll, rotationLerpPct);
+                Yaw = Mathf.Lerp(Yaw, target.Yaw, rotationLerpPct);
+                Pitch = Mathf.Lerp(Pitch, target.Pitch, rotationLerpPct);
+                Roll = Mathf.Lerp(Roll, target.Roll, rotationLerpPct);
                 
-                x = Mathf.Lerp(x, target.x, positionLerpPct);
-                y = Mathf.Lerp(y, target.y, positionLerpPct);
-                z = Mathf.Lerp(z, target.z, positionLerpPct);
+                X = Mathf.Lerp(X, target.X, positionLerpPct);
+                Y = Mathf.Lerp(Y, target.Y, positionLerpPct);
+                Z = Mathf.Lerp(Z, target.Z, positionLerpPct);
             }
 
             public void UpdateTransform(Transform t)
             {
-                t.eulerAngles = new Vector3(pitch, yaw, roll);
-                t.position = new Vector3(x, y, z);
+                t.eulerAngles = new Vector3(Pitch, Yaw, Roll);
+                t.position = new Vector3(X, Y, Z);
             }
         }
         
-        CameraState m_TargetCameraState = new CameraState();
-        CameraState m_InterpolatingCameraState = new CameraState();
+        CameraState _mTargetCameraState = new CameraState();
+        CameraState _mInterpolatingCameraState = new CameraState();
 
         [Header("Movement Settings")]
         [Tooltip("Exponential boost factor on translation, controllable by mouse wheel.")]
@@ -78,8 +78,8 @@ namespace UnityTemplateProjects
 
         void OnEnable()
         {
-            m_TargetCameraState.SetFromTransform(transform);
-            m_InterpolatingCameraState.SetFromTransform(transform);
+            _mTargetCameraState.SetFromTransform(transform);
+            _mInterpolatingCameraState.SetFromTransform(transform);
         }
 
         Vector3 GetInputTranslationDirection()
@@ -167,15 +167,15 @@ namespace UnityTemplateProjects
             // TODO: make the new input system work
 #endif
 
-            m_TargetCameraState.Translate(translation);
+            _mTargetCameraState.Translate(translation);
 
             // Framerate-independent interpolation
             // Calculate the lerp amount, such that we get 99% of the way to our target in the specified time
             var positionLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / positionLerpTime) * Time.deltaTime);
             var rotationLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / rotationLerpTime) * Time.deltaTime);
-            m_InterpolatingCameraState.LerpTowards(m_TargetCameraState, positionLerpPct, rotationLerpPct);
+            _mInterpolatingCameraState.LerpTowards(_mTargetCameraState, positionLerpPct, rotationLerpPct);
 
-            m_InterpolatingCameraState.UpdateTransform(transform);
+            _mInterpolatingCameraState.UpdateTransform(transform);
         }
     }
 
